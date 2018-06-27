9dab14ee7adfbff60eaec2930f83c9ea3f1e8f4c
sunpasup <sunpasup@cisco.com>
2018-06-27 14:21:02 -0700

    second One

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { sampleData} from '../twitterData';
+import {AuthService, SocialUser} from 'angularx-social-login';
+
 
 
 
@@ -12,14 +14,24 @@ import { sampleData} from '../twitterData';
 
 export class IconsComponent implements OnInit {
 
+
     public admin: any = false;
 
     public twitterData: any = sampleData;
 
-    constructor() {
+    user: SocialUser;
+
+    constructor(private authService: AuthService) { }
+
+    ngOnInit() {
+        this.authService.authState.subscribe((user) => {
+            this.user = user;
+            console.log(user);
+        });
     }
 
-  ngOnInit() {
-  }
+
+
+
 
 }
